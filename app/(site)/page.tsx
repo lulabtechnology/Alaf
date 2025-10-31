edca578f57b96e455c78b9a0d24568a663d58996
lulabtechnology <131550504+lulabtechnology@users.noreply.github.com>
2025-10-31 15:49:19 -0600

    Create page.tsx

---
+++ app/(site)/page.tsx
@@ -0,0 +1,32 @@
+import { Navbar } from './components/navbar';
+import { Hero } from './components/hero';
+import { Benefits } from './components/benefits';
+import { WhyUs } from './components/why-us';
+import { About } from './components/about';
+import { Services } from './components/services';
+import { Successes } from './components/successes';
+import { Testimonials } from './components/testimonials';
+import { FAQ } from './components/faq';
+import { Contact } from './components/contact';
+import { Footer } from './components/footer';
+
+
+export default function Page() {
+return (
+<>
+<Navbar />
+<main>
+<Hero />
+<Benefits />
+<WhyUs />
+<About />
+<Services />
+<Successes />
+<Testimonials />
+<FAQ />
+<Contact />
+</main>
+<Footer />
+</>
+);
+}
