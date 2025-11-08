6a16fdbb5363714c8f6cd5f54cb094cda1199321
lulabtechnology <131550504+lulabtechnology@users.noreply.github.com>
2025-11-08 11:13:41 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    // tamaños para desktop grandes (evita que Next genere imágenes chicas)
-    deviceSizes: [640, 768, 1024, 1280, 1536, 1920, 2560, 3200],
+    deviceSizes: [640, 768, 1024, 1280, 1536, 1920, 2560],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384, 512],
     formats: ["image/avif", "image/webp"],
-    // Si sigues viendo /_next/image devolviendo avif muy chico,
-    // puedes desactivar la optimización del HERO (ver hero.tsx).
   },
 };
 module.exports = nextConfig;
