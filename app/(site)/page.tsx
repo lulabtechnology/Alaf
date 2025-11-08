22f0ab359462d937f0b9f58b7f111fe63322ea3e
lulabtechnology <131550504+lulabtechnology@users.noreply.github.com>
2025-11-08 12:18:32 -0600

    Update page.tsx

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,32 +1,31 @@
-import { Navbar } from './components/navbar';
-import { Hero } from './components/hero';
-import Benefits from "./components/benefits";
-import { WhyUs } from './components/why-us';
-import { About } from './components/about';
-import { Services } from './components/services';
-import { Successes } from './components/successes';
-import { Testimonials } from './components/testimonials';
-import { FAQ } from './components/faq';
-import { Contact } from './components/contact';
-import { Footer } from './components/footer';
-
+import { Navbar } from "./components/navbar";
+import { Hero } from "./components/hero";
+import { About } from "./components/about";
+import { WhyUs } from "./components/why-us";
+import { Benefits } from "./components/benefits";
+import { Services } from "./components/services"; // ahora "Modalidades"
+import { Gallery } from "./components/gallery";   // NUEVO
+import { Testimonials } from "./components/testimonials";
+import { FAQ } from "./components/faq";
+import { Contact } from "./components/contact";
+import { Footer } from "./components/footer";
 
 export default function Page() {
-return (
-<>
-<Navbar />
-<main>
-<Hero />
-<Benefits />
-<WhyUs />
-<About />
-<Services />
-<Successes />
-<Testimonials />
-<FAQ />
-<Contact />
-</main>
-<Footer />
-</>
-);
+  return (
+    <>
+      <Navbar />
+      <Hero />
+
+      {/* Orden solicitado */}
+      <About />           {/* Acerca de ALAF */}
+      <WhyUs />           {/* Por qué elegir ALAF */}
+      <Benefits />        {/* Prioriza tu aprendizaje */}
+      <Services />        {/* Modalidades ALAF */}
+      <Gallery />         {/* Galería (reemplaza Éxitos académicos) */}
+      <Testimonials />
+      <FAQ />
+      <Contact />
+      <Footer />
+    </>
+  );
 }
